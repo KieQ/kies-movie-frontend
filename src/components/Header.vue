af6024ec6b478404e484ac3dc735d4a5aa18346d
sunbo.kie <sunbo.kie@bytedance.com>
2022-12-08 18:00:07 +0800

    [ADD] finish header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -41,26 +41,28 @@
             <img class="rounded-full w-6" src="../assets/img/logo.png" alt="profile"/>
           </button>
 
-          <ul class="flex flex-col text-center items-start justify-center p-0 -ml-32 absolute" :class="{hidden:!show_profile_menu}" v-if="user_info.login" id="profileMenu">
-            <li class="bg-gray-100 hover:bg-blue-300 w-40 h-10 flex flex-col justify-center items-start px-3 cursor-pointer">
-              <span>
-                {{english?"Dashboard":"控制板"}}
-              </span>
-            </li>
-            <li class="bg-gray-100 hover:bg-blue-300 w-40 h-10 flex flex-col justify-center items-start px-3 cursor-pointer">
-              <span>
-                {{english?"Setting":"设置"}}
-              </span>
-            </li>
-            <li>
-              <hr class="w-5/6 mx-auto"/>
-            </li>
-            <li class="bg-gray-100 hover:bg-blue-300 w-40 h-10 flex flex-col justify-center items-start px-3 cursor-pointer">
-              <span>
-                {{english?"Logout":"退出"}}
-              </span>
-            </li>
-          </ul>
+          <Transition name="slide-fade">
+            <ul class="flex flex-col text-center items-start justify-center p-0 -ml-32 absolute" v-if="show_profile_menu">
+              <li class="bg-gray-100 hover:bg-blue-300 w-40 h-10 flex flex-col justify-center items-start px-3 cursor-pointer">
+                <a class="w-full text-start" href="/dashboard">
+                  {{english?"Dashboard":"控制板"}}
+                </a>
+              </li>
+              <li class="bg-gray-100 hover:bg-blue-300 w-40 h-10 flex flex-col justify-center items-start px-3 cursor-pointer">
+                <a class="w-full text-start" href="/setting">
+                  {{english?"Setting":"设置"}}
+                </a>
+              </li>
+              <li>
+                <hr class="w-5/6 mx-auto"/>
+              </li>
+              <li class="bg-gray-100 hover:bg-blue-300 w-40 h-10 flex flex-col justify-center items-start px-3 cursor-pointer">
+                <a class="w-full text-start" href="/logout">
+                  {{english?"Logout":"退出"}}
+                </a>
+              </li>
+            </ul>
+          </Transition>
         </div>
 
     </div>
@@ -70,15 +72,15 @@
 <!--navbar for Mobile-->
   <nav class="flex flex-col md:hidden not-select justify-between items-center bg-gray-300/50 absolute w-full py-2 px-0 space-x-2">
     <div class="flex flex-row justify-between items-center w-full">
-      <div class="w-16 ml-2">
+      <div class="w-16 ml-2 duration-300 ease-in-out transition grow" :class="{move_to_center:show_mobile_menu}">
         <a href="/">
-          <img src="../assets/img/logo.png" class="w-8 mx-auto brightness-90 hover:brightness-100" alt="logo"/>
+          <img src="../assets/img/logo.png" class="w-8 ml-2 brightness-90 hover:brightness-100" alt="logo"/>
         </a>
       </div>
 
       <div class="w-16 cursor-pointer" @click="trigger_mobile_menu">
           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars"
-               class="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+               class="w-6 duration-300 ease-in-out transition" :class="{rotate :show_mobile_menu}" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
             <path fill="currentColor"
                   d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
             </path>
@@ -86,21 +88,50 @@
       </div>
     </div>
 
-    <div class="flex flex-col" :class="{hidden:hide_mobile_menu}" id="mobileMenu">
-      <ul class="flex flex-col pl-0 list-style-none mr-auto">
-        <li class="p-2">
-          <a class="text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="#">
-            {{english?"Movie":"电影"}}
-          </a>
-        </li>
-        <li class="p-2">
-          <a class="text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="#">
-            {{english?"About":"关于"}}
-          </a>
-        </li>
-      </ul>
-    </div>
-
+    <Transition name="slide-fade">
+      <div class="flex flex-col" v-if="show_mobile_menu" id="mobileMenu">
+        <ul class="flex flex-col pl-0 list-style-none mr-auto">
+          <li class="p-2 w-60 text-center">
+            <div class="h-full flex flex-row items-center justify-center w-full">
+              <div class="flex flex-row items-center form-check form-switch space-x-2">
+                <input class="form-check-input appearance-none w-16 -ml-10 rounded-full float-left h-8 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="languageSwitch" v-model="english">
+                <label class="form-check-label inline-block text-gray-800" for="flexSwitchCheckDefault">{{english?"EN":"中"}}</label>
+              </div>
+            </div>
+          </li>
+          <li class="p-2 w-60 text-center">
+            <a class="text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="/movie">
+              {{english?"Movie":"电影"}}
+            </a>
+          </li>
+          <li class="p-2 w-60 text-center">
+            <a class="text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="/about">
+              {{english?"About":"关于"}}
+            </a>
+          </li>
+          <li class="p-2 w-60 flex flex-row justify-center" v-if="!user_info.login">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" @click="unLogin_profile_click">
+              {{ english?"LOG IN":"登陆" }}
+            </button>
+          </li>
+          <li class="p-2 w-60 text-center" v-if="user_info.login">
+            <a class="text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="/dashboard">
+              {{english?"Dashboard":"控制板"}}
+            </a>
+          </li>
+          <li class="p-2 w-60 text-center" v-if="user_info.login">
+            <a class="text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="/setting">
+              {{english?"Setting":"设置"}}
+            </a>
+          </li>
+          <li class="p-2 w-60 text-center" v-if="user_info.login">
+            <a class="text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="/logout">
+              {{english?"Logout":"退出"}}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </Transition>
   </nav>
 </template>
 
@@ -109,10 +140,12 @@
   import {update_user_info, user_info, listen_click} from "@/utility/utility";
   import {useRouter} from "vue-router";
 
+  //Global Variable
   const english = ref(false);
   const router = useRouter();
   const show_profile_menu = ref(false);
 
+  //Mount
   onMounted(()=>{
     update_user_info();
     user_info.login=true;
@@ -128,16 +161,46 @@
     });
   })
 
-
-  const hide_mobile_menu = ref(true);
-  function trigger_mobile_menu(){
-    hide_mobile_menu.value = !hide_mobile_menu.value;
+  //Click Not Login Profile Mobile
+  function unLogin_profile_click(){
+    router.push("/login");
   }
+
+  //Show Mobile Menu
+  const show_mobile_menu = ref(false);
+  function trigger_mobile_menu(){
+    show_mobile_menu.value = !show_mobile_menu.value;
+  }
+
+
+
 
 </script>
 
 <style scoped>
 .not-select {
   user-select: none;
+}
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(-20px);
+  opacity: 0;
+}
+
+.rotate {
+  transform: rotate(-90deg);
+}
+
+.move_to_center{
+  transform: translateX(50%) translateX(8px);
 }
 </style>
