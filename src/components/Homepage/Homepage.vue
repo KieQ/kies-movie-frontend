a1015d3a60bc551c9322b0e7b1486b338efa0c7f
sunbo.kie <sunbo.kie@bytedance.com>
2022-12-11 00:14:56 +0800

    [REMOVE] remove test data

--- src/components/Homepage/Homepage.vue
+++ src/components/Homepage/Homepage.vue
@@ -48,7 +48,7 @@ import {onMounted, reactive, ref} from "vue";
 import {english} from "@/utility/language";
 import {wrap_if_too_long} from "@/utility/utility";
 
-  const content = reactive([{"poster_image":"https://image.tmdb.org/t/p/original/34mHYXohSsyxu7QLJTG0IYW7zMr.jpg","background_image":"https://image.tmdb.org/t/p/original/4rpGuAgfIBVSytvPoYYJfFjaxpF.jpg","title":"小门神","content":"故事发生在当代的人间江南小镇和神界。 门神神荼和郁垒是两兄弟。因为近年人间不再关注神仙，神界经济萧条，门神、土地爷、八仙等等小神们面临下岗失业的危险。门神郁垒决定去人间，做一番惊天动地的事情，证明门神的价值。郁垒和神荼先后来到人间，遇到了小镇上的单亲母女小英和雨儿，之后发生了一系列意想不到的有趣惊险故事……","value":""},{"poster_image":"https://image.tmdb.org/t/p/original/guvDsSrSYtOHu4EWbUK55NaMZqH.jpg","background_image":"https://image.tmdb.org/t/p/original/ptgoTztfxbz0qFo129FwyisORRs.jpg","title":"捉妖记","content":"　　山雄伟，海辽阔，经奇幻。自古以来人妖共存于世，人欲主天下，妖遂被驱逐入山。其后历朝，妖偶有越界，皆由捉妖天师秘密处理。老妖王辞世，妖界大乱，众妖越界流窜，一时间世间人妖难辨。在偏安深山的永宁村中，村长宋天荫（井柏然 饰）偶遇一路逐妖而来的菜鸟天师霍小岚（白百何 饰），在意外共度一夜后，被妖后托胎，随即生下了小妖王胡巴。为了自己的私心，小岚带着天荫一路同行前往顺天府，保护他躲过各种妖怪，也渐渐与天荫及胡巴产生了感情。然而为时已晚，胡巴已落入坏人之手，为营救胡巴，小岚与天荫在众人的帮助下勇闯登仙楼，与大反派葛千户（钟汉良 饰）展开一场殊死对决……人妖相克，究竟谁主天下？","value":""},{"poster_image":"https://image.tmdb.org/t/p/original/77QFDqbgiLVkdp9lDDlsp6Lmuen.jpg","background_image":"https://image.tmdb.org/t/p/original/qwBpO7Lal9tjElcazD922wzQDNE.jpg","title":"脱口秀大会","content":"　　《脱口秀大会》是由企鹅影视、上海笑果文化传媒有限公司联合出品的脱口秀高手对战节目。","value":""}])
+  const content = reactive([])
 
   const active_idx = ref(0)
   const move_left = ref(true);
