082e3917b97990a1f789aa960ec243427c438c28
sunbo.kie <sunbo.kie@bytedance.com>
2022-12-11 09:48:41 +0800

    [ADD] adapt mobile

--- src/components/Homepage/Homepage.vue
+++ src/components/Homepage/Homepage.vue
@@ -2,9 +2,9 @@
   <div class="h-full flex flex-row justify-center space-x-0 relative">
     <TransitionGroup :name="move_left?'slide_left':'slide_right'">
       <template v-for="(item, idx) of content" :key="idx">
-        <div class="absolute top-0 h-full overflow-hidden flex flex-col justify-end not-select" v-if="idx===active_idx">
+        <div class="absolute top-0 h-1/2 md:h-full overflow-hidden flex flex-col justify-end not-select" v-show="idx===active_idx">
           <img class="object-cover -z-10 absolute w-full h-full" :src="item.background_image" alt="">
-          <div class="flex flex-col px-32 py-24">
+          <div class="flex flex-col px-10 md:px-32 py-12 md:py-24">
             <h1 class="text-white text-lg md:text-2xl">{{item.title}}</h1>
             <p class="text-white text-sm md:text-lg">{{content_text(item.content)}}</p>
           </div>
@@ -13,14 +13,14 @@
     </TransitionGroup>
   </div>
   <!-- Button-->
-  <div class="absolute h-full w-12 left-8 flex flex-col justify-center z-0">
+  <div class="absolute h-1/2 md:h-full w-12 left-8 flex flex-col justify-center z-0">
     <button class="text-white/50 hover:text-white/70" @click="click_left">
       <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-left-circle w-full" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
       </svg>
     </button>
   </div>
-  <div class="absolute h-full w-12 right-8 flex flex-col justify-center">
+  <div class="absolute h-1/2 md:h-full w-12 right-8 flex flex-col justify-center">
     <button class="text-white/50 hover:text-white/70" @click="click_right">
       <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-right-circle w-full" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
@@ -29,7 +29,7 @@
   </div>
 
   <!--Bottom Button-->
-  <div class="absolute w-full bottom-16 bg-white/0 flex flex-row justify-center items-center space-x-1">
+  <div class="absolute w-full bottom-1/2 md:bottom-16 bg-white/0 flex flex-row justify-center items-center space-x-1">
     <template v-for="idx in content.length">
       <button class="w-10 h-10 cursor-pointer" @click="change_active_index(idx)">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-record" viewBox="0 0 16 16" v-if="active_idx===idx-1">
